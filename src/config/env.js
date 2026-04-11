d147b544810bff3f9385631bbfacf6f8aa9074d0
unknown <Saikrishna93915>
2026-04-11 15:16:56 +0530

    fix: convert env.js to ES modules and update configuration

--- src/config/env.js
+++ src/config/env.js
@@ -1,6 +1,7 @@
-require("dotenv").config();
+import dotenv from "dotenv";
+dotenv.config();
 
-module.exports = {
+export default {
   MONGO_URI: process.env.MONGO_URI,
   JWT_SECRET: process.env.JWT_SECRET || "supersecurekey",
   PORT: process.env.PORT || 8080
